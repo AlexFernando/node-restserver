e353f2645e6482c53c4a1f96cc7a06e4f8b67a0f
alexfernando <alex.fernando.801@gmail.com>
2020-06-10 01:16:42 +0000

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,40 +1,21 @@
 require('./config/config');
 
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 
 app.use(express.urlencoded({ extended: true })) // serializar url, enviar la info de mis request a mi server 
+
+app.use(require('./routes/usuario'))
  
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
-})
+mongoose.connect(process.env.URLDB, (err, res) => {
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
+    if (err) throw err;
 
-    if( body.nombre === undefined ) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        })
-    }
-})
+    console.log('Base de datos ONLINE')
 
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id; // este params.id es el id de la url, tiene que ser el mismo nombre
-    res.json({
-        id
-    });
-})
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-})
+});
  
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT)
